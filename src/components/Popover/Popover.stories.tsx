585484ddc00ab4db4c75d52b4555544279194770
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-10-01 20:57:10 +0200

    updated popover component

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -6,7 +6,8 @@ const meta: Meta<typeof Popover> = {
   component: Popover,
   decorators: [
     (Story) => (
-      <div className="w-[400px]">
+      <div className="w-[600px] h-[50px] relative bg-teal-primary mt-16">
+        <div>This is placeholder content</div>
         <Story />
       </div>
     ),
@@ -17,8 +18,46 @@ export default meta;
 
 type Story = StoryObj<typeof Popover>;
 
-export const Default: Story = {
+// export const Default: Story = {
+//   args: {
+//     children: (
+//       <p>
+//         Hello worldHello worldHello worldHello worldHello worldHello worldHello
+//         world Hello world Hello world Hello world Hello world Hello world Hello
+//         world Hello world Hello world Hello world Hello world Hello world
+//       </p>
+//     ),
+//     className: "ml-[-4em] max-w-[100px] flex flex-wrap",
+//   },
+// };
+
+
+
+export const Top: Story = {
   args: {
-   
+    children: <p>Hello world</p>,
+    placement: "top",
+    arrow: "bottom",
+    className: "ml-[-4em]",
   },
 };
+
+export const Bottom: Story = {
+  args: {
+    children: <p>Hello world</p>,
+    placement: "bottom",
+    arrow: "top",
+    className: "ml-[-4em]",
+  },
+};
+
+
+export const Right: Story = {
+  args: {
+    children: <p>Hello world</p>,
+    placement: "right",
+    // arrow: "top",
+    // className: "ml-[-4em]",
+  },
+};
+
--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -5,21 +5,57 @@ import { twMerge } from "tailwind-merge";
 
 export type PopoverVariantProps = VariantProps<typeof PopoverVariants>
 
+export interface PopoverProps extends PopoverVariantProps {
+  children: JSX.Element | React.ReactNode;
+  className?: string;
+  arrow?: "top" | "bottom" | "left"| "right";
+}
 
-const PopoverVariants = cva("", {
-    variants:{},
-
-    defaultVariants:{
-
+const PopoverVariants = cva(
+  " absolute flex flex-col items-center z-40 p-2 bg-background-light border border-border-primary rounded shadow-sm w-fit md:w-max h-fit ",
+  {
+    variants: {
+      placement: {
+        top: "top-[-55px] left-[50%]",
+        bottom: "bottom-[-55px] left-[50%]",
+        left: "",
+        right: "right-0",
+      },
     },
-});
+
+    defaultVariants: {
+      placement: "top",
+    },
+  }
+);
 
 
 
-export default function Popover({}) {
+export default function Popover({children, className, placement, arrow}:PopoverProps) {
 
     return (
-        <div>hello world</div>
-    )
+      <div
+        className={`${twMerge(
+          PopoverVariants({ className, placement }),
+          "w-[100%]"
+        )}`}
+      >
+        {arrow && (
+          <div className="flex justify-center w-[100%]">
+            <div
+              className={`${
+                arrow === "top"
+                  ? "arrow-top"
+                  : arrow === "bottom"
+                  ? "arrow-bottom"
+                  : ""
+              } absolute bg-background-light w-[12px] h-[12px] border-border-primary `}
+            ></div>
+          </div>
+        )}
+
+        {children}
+      </div>
+    );
 
 }
